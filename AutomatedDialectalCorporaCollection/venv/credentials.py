41a412db40674084db72da4de84f5781b8d1952f
Maab-Ibrahim <48053219+Maab-Ibrahim@users.noreply.github.com>
2019-09-18 10:36:49 +0100

    Update credentials.py

--- AutomatedDialectalCorporaCollection/venv/credentials.py
+++ AutomatedDialectalCorporaCollection/venv/credentials.py
@@ -1,5 +1,5 @@
 ##############################Twitter API Credentials################################
-consumer_key = "nqFuSCpNtg8aYC9Zou8LUBCXB"
-access_token = "823786350-TRxumGiR1pLkZJt2JUS7MLPOv0n5Z1OcXXjuMPN0"
-consumer_secret = "c6oSTY14COx5zRMKodBFxR1fzeNlUYBt2H5wZHRC14HFJZZCQE"
-access_token_secret = "Q4Z8FP9NuPOJxsEI1Qyzjd6PlthzhWZBLCGJmuBSdtrfh"
+consumer_key = XXXXX
+access_token = XXXXX
+consumer_secret = XXXXX
+access_token_secret = XXXXX
